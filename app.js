9e6a1e26852865573db8bc80094a03e744d3949d
sampath kumar <sampathkumar@sampaths-MacBook-Air.local>
2021-01-26 15:14:45 -0500

    Basics of JS

---
+++ app.js
@@ -0,0 +1,140 @@
+var nam = 'Sampath';
+console.log(nam);
+
+//var vs let
+/*
+The main difference between var and let is the Scope of the variable.
+
+Var is Function scoped and let is block scoped.
+
+
+Const is reference type and has to be initialized.
+Creates a new object and can be mutated but cannot be reassigned
+
+
+*/
+
+//Dynamically typed and primitive types, reference types
+/*
+JavaScript is Dynamically typed. 
+The same variable can hold many values and types are associated with values not variables.
+There are some supersets of JS like TypeScript that allow static typing
+
+Null will return of type object but in reailty is a preimitive type
+
+
+const n= " Sampath";
+console.log(typeof n);
+
+const car = null;
+console.log(typeof car);
+
+let und = undefined;
+console.log(typeof und);
+
+const hobbies = ['music', 'dance'];
+const address = {
+    city :'manchester',
+    zip: '03102'
+}
+console.log(typeof address);
+
+*/
+// Type conversion
+/*var val = '5';
+val = Number(val);
+console.log(val.toFixed());// Helps to print decimal values
+val = Number(null);
+console.log(val);// gives 0
+val = Number('hellp');
+console.log(val);// prints NaN
+*/
+
+
+//Template literals
+/*var age = 50;
+var city = "Villanova";
+var gender = "male";
+
+let html;
+//without template literals
+html = '<ul>' + 
+    '<li>Name: ' + nam +  '</li>' + 
+    '<li>Age: ' + age +  '</li>' + 
+    '<li>City: ' + city +  '</li>' + 
+    '<li>Gender: ' +  gender +  '</li>' + 
+    '</ul>';
+
+//with template literals
+html2 =`
+<ul>
+<li>Name: ${nam}</li>
+<li>Age: ${age}</li>
+<li>City: ${city}</li>
+<li>Gender: ${gender}</li>
+
+
+</ul>
+
+
+`
+
+document.body.innerHTML = html2;
+*/
+
+
+//Arrays fun methods
+/*
+val numbers = [20,30, 405];
+numbers.push(250); // add at the end
+numbers.unshift(120);// add at the front
+numbers.shift();// remove from front
+numbers.splice(1, 1); // remove elements between indices
+
+*/
+
+
+//Difference between == and ===
+/*
+const num = '100';
+
+if(num === 100){
+    console.log('correct');
+}
+else{
+    console.log('incorrect');
+}
+
+
+*/
+
+
+//function declarations and expression
+
+function greet(firstname , lastname){
+    return firstname
+    + " " + lastname;
+}
+
+console.log(greet('sampath', 'kumar'));
+console.log(greet());//returns undefined undefined
+
+// to avoid that we can set some default values for fistname and lastname like
+function greet(firstName = 'John', lastName = 'Doe'){
+    return firstName + " " + lastName;
+}
+console.log(greet());
+
+
+const square = function(x =3){
+    return x*x;
+};
+
+console.log(square(5));
+
+//Immediately invokable function expressions iifes
+
+(function(){
+    console.log("IIFEs ran..");
+})();
+
